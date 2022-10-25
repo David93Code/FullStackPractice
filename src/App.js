14dd71b511e54bdb1eba8aa27ba6f2cb20aa745f
david armeni <david.armeni1993@gmail.com>
2022-10-25 23:00:06 +0300

    handling arrays exercise

--- src/App.js
+++ src/App.js
@@ -26,27 +26,49 @@
 
 import { useState } from "react";
 
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+//   const increaseByOne = () => setCounter(counter + 1);
+//   const decreaseByOne = () => setCounter(counter - 1);
+//   const setToZero = () => setCounter(0);
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="plus" />
+//       <Button onClick={setToZero} text="zero" />
+//       <Button onClick={decreaseByOne} text="minus" />
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClicks = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClicks}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   );
 };
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 export default App;
