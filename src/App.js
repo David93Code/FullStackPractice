7a3f0827c322bf0f6c6a476d77e9a81ed6711477
David Armeni <david.armeni1993@gmail.com>
2022-11-03 10:44:30 +0200

    commit practise notes

--- src/App.js
+++ src/App.js
@@ -1,101 +1,18 @@
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age;
+import Note from "./components/Note";
 
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   const name = "Peter";
-//   const age = 10;
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={name} age={age} />
-//     </div>
-//   );
-// };
-
-import { useState } from "react";
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0);
-//   const increaseByOne = () => setCounter(counter + 1);
-//   const decreaseByOne = () => setCounter(counter - 1);
-//   const setToZero = () => setCounter(0);
-//   return (
-//     <div>
-//       <Display counter={counter} />
-//       <Button onClick={increaseByOne} text="plus" />
-//       <Button onClick={setToZero} text="zero" />
-//       <Button onClick={decreaseByOne} text="minus" />
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   const [left, setLeft] = useState(0);
-//   const [right, setRight] = useState(0);
-//   const [allClicks, setAll] = useState([]);
-
-//   const handleLeftClick = () => {
-//     setAll(allClicks.concat("L"));
-//     setLeft(left + 1);
-//   };
-
-//   const handleRightClick = () => {
-//     setAll(allClicks.concat("R"));
-//     setRight(right + 1);
-//   };
-
-//   return (
-//     <div>
-//       {left}
-//       <Button handleClick={handleLeftClick} text="left" />
-//       <Button handleClick={handleRightClick} text="right" />
-//       {right}
-//       <History allClicks={allClicks} />
-//     </div>
-//   );
-// };
-
-// const Display = ({ counter }) => <div>{counter}</div>;
-
-// const Button = ({ handleClick, text }) => (
-//   <button onClick={handleClick}>{text}</button>
-// );
-
-// const History = (props) => {
-//   if (props.allClicks.length === 0) {
-//     return <div>the app is used by pressing the buttons</div>;
-//   }
-//   return <div>button press history: {props.allClicks.join(" ")}</div>;
-// };
-
-const App = () => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = (newValue) => () => {
-    console.log("value now", newValue);
-    setValue(newValue);
-  };
-
+const App = ({ notes }) => {
   return (
     <div>
-      {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
+  const result = notes.map((note) => note.content);
+  console.log(result);
 };
 
 export default App;
